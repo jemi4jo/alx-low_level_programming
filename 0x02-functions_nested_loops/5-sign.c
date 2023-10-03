fb363666d5c875867c72a154148f0d8a6261ae60
jemi4jo <140697222+jemi4jo@users.noreply.github.com>
2023-10-03 17:22:36 +0800

    New Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,5 +13,10 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	} else if (n < 0){
+		_putchar('-');
+		return (-1);
+	} else {
+		_putchar('0');
+		return (0);
 }
